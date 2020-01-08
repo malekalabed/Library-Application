001502ccefd67307306fd362d60c26fb9a71fda4
malekalabed <57950278+malekalabed@users.noreply.github.com>
2020-01-08 13:21:13 +0100

    Admin Service Complete

--- src/com/company/services/Admin_Service.java
+++ src/com/company/services/Admin_Service.java
@@ -3,27 +3,24 @@ package com.company.services;
 import com.company.interfaces.I_LoginService;
 import com.company.models.Admin;
 import com.company.repositories.Data_Repository;
-
 import java.util.List;
-
 
 public class Admin_Service implements I_LoginService<Admin> {
 
     private List<Admin>admins;
-    private Data_Repository data_repository = new Data_Repository();
-
 
     public Admin_Service() {
-
-    }
-
-
-    public List<Admin> getAdmins() {
-        return admins;
+        Data_Repository data_repository = new Data_Repository();
+        admins = data_repository.getAdminsList();
     }
 
     @Override
     public boolean userAuthentication(Admin user) {
+        for (Admin m: admins){
+            if (m.getSsn().equals(user.getSsn()) && m.getPassword().equals(user.getPassword()) && m.getAdminCode().equalsIgnoreCase(user.getAdminCode())){
+                return true;
+            }
+        }
         return false;
     }
 }
