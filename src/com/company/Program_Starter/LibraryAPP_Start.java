1d8e4bc906f5a4fd6090aec691b4e4e28f1ae1f0
malekalabed <57950278+malekalabed@users.noreply.github.com>
2020-01-08 13:23:00 +0100

    Adjusted the library app start based on the start controller

--- src/com/company/Program_Starter/LibraryAPP_Start.java
+++ src/com/company/Program_Starter/LibraryAPP_Start.java
@@ -1,15 +1,14 @@
 package com.company.Program_Starter;
 
-import com.company.controller.Library_Admin_Controller;
-
+import com.company.controller.Library_Start_Controller;
 
 public class LibraryAPP_Start {
 
     public static  void  main(String []args) throws InterruptedException {
+        Library_Start_Controller start_controller = new Library_Start_Controller();
 
-        Library_Admin_Controller admin_controller = new Library_Admin_Controller();
+        start_controller.mainMenu();
 
-        admin_controller.mainMenu();
 
     }
 }
